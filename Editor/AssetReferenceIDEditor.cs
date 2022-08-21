4ac2ce3c65f1c05f706be5cd72541ba529bf7b6d
etienneR <55545442+Nyert@users.noreply.github.com>
2022-08-21 22:46:20 +0200

    Update AssetReferenceIDEditor.cs

--- Editor/AssetReferenceIDEditor.cs
+++ Editor/AssetReferenceIDEditor.cs
@@ -1,19 +1,19 @@
 ﻿using Drboum.Utilities.Runtime.EditorHybrid;
-using Drboum.Utilities.Runtime.EditorHybrid;
 using UnityEditor;
 using UnityEngine;
-namespace GameProject.Editors;
+namespace Drboum.Utilities.Editor;
 
 [CustomEditor(typeof(AssetReferenceID), true)]
-public class AssetReferenceIDEditor : Editor {
+public class AssetReferenceIDEditor : Editor
+{
     public override void OnInspectorGUI()
     {
         var assetId = target as AssetReferenceID;
         if ( !assetId.IsNull() )
         {
             EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField(nameof(AssetReferenceID.Guid),GUILayout.Width(50));
-            EditorGUILayout.SelectableLabel(assetId.Guid.ToString(),GUILayout.ExpandWidth(true));
+            EditorGUILayout.LabelField(nameof(AssetReferenceID.Guid), GUILayout.Width(50));
+            EditorGUILayout.SelectableLabel(assetId.Guid.ToString(), GUILayout.ExpandWidth(true));
             EditorGUILayout.EndHorizontal();
         }
         base.OnInspectorGUI();
