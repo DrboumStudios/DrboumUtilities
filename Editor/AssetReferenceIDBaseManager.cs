7e747d6dddf41ee248f3a2fae0200d18b931c97a
etienneR <55545442+Nyert@users.noreply.github.com>
2022-09-10 18:30:08 +0200

    moved assetpostprocessor to its own file

--- Editor/AssetReferenceIDBaseManager.cs
+++ Editor/AssetReferenceIDBaseManager.cs
@@ -115,35 +115,4 @@ namespace Drboum.Utilities.Editor
             AssetReferenceIDBaseManager<TAssetID>.Instance.FixAssetIDIfInvalid(instance);
         }
     }
-    class MyAllPostprocessor : AssetPostprocessor
-    {
-        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
-        {
-            for ( var index = 0; index < importedAssets.Length; index++ )
-            {
-                string str = importedAssets[index];
-                var assetReferenceID = AssetDatabase.LoadAssetAtPath<AssetReferenceID>(str);
-                if ( assetReferenceID != null )
-                {
-                    assetReferenceID.FixAssetIDIfInvalid();
-                }
-                LogHelper.LogDebugMessage($"Reimported Asset of type {assetReferenceID} : {str}");
-            }
-            
-            foreach (string str in deletedAssets)
-            {
-                LogHelper.LogDebugMessage("Deleted Asset: " + str);
-            }
-
-            for (int i = 0; i < movedAssets.Length; i++)
-            {
-                LogHelper.LogDebugMessage("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
-            }
-
-            if (didDomainReload)
-            {
-                LogHelper.LogDebugMessage("Domain has been reloaded");
-            }
-        }
-    }
 }
---
+++ Editor/MyAllPostprocessor.cs
@@ -0,0 +1,36 @@
+﻿using Drboum.Utilities.Runtime.EditorHybrid;
+using UnityEditor;
+namespace Drboum.Utilities.Editor
+{
+    class MyAllPostprocessor : AssetPostprocessor
+    {
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
+        {
+            for ( var index = 0; index < importedAssets.Length; index++ )
+            {
+                string str = importedAssets[index];
+                var assetReferenceID = AssetDatabase.LoadAssetAtPath<AssetReferenceID>(str);
+                if ( assetReferenceID != null )
+                {
+                    assetReferenceID.FixAssetIDIfInvalid();
+                }
+                LogHelper.LogDebugMessage($"Reimported Asset of type {assetReferenceID} : {str}");
+            }
+            
+            foreach (string str in deletedAssets)
+            {
+                LogHelper.LogDebugMessage("Deleted Asset: " + str);
+            }
+
+            for (int i = 0; i < movedAssets.Length; i++)
+            {
+                LogHelper.LogDebugMessage("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
+            }
+
+            if (didDomainReload)
+            {
+                LogHelper.LogDebugMessage("Domain has been reloaded");
+            }
+        }
+    }
+}
