088ce7e3cbd31db76ca9ef959e944332b125f985
etienneR <55545442+Nyert@users.noreply.github.com>
2022-09-10 18:36:02 +0200

    Update MyAllPostprocessor.cs

--- Editor/MyAllPostprocessor.cs
+++ Editor/MyAllPostprocessor.cs
@@ -2,9 +2,9 @@
 using UnityEditor;
 namespace Drboum.Utilities.Editor
 {
-    class MyAllPostprocessor : AssetPostprocessor
+    class AssetReferenceIDAllPostprocessor : AssetPostprocessor
     {
-        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload)
         {
             for ( var index = 0; index < importedAssets.Length; index++ )
             {
@@ -14,22 +14,6 @@ namespace Drboum.Utilities.Editor
                 {
                     assetReferenceID.FixAssetIDIfInvalid();
                 }
-                LogHelper.LogDebugMessage($"Reimported Asset of type {assetReferenceID} : {str}");
-            }
-            
-            foreach (string str in deletedAssets)
-            {
-                LogHelper.LogDebugMessage("Deleted Asset: " + str);
-            }
-
-            for (int i = 0; i < movedAssets.Length; i++)
-            {
-                LogHelper.LogDebugMessage("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
-            }
-
-            if (didDomainReload)
-            {
-                LogHelper.LogDebugMessage("Domain has been reloaded");
             }
         }
     }
