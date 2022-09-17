8bb6f489c381544cc9f5882ff4ba8e51fbcc88e9
etienneR <55545442+Nyert@users.noreply.github.com>
2022-09-17 17:18:48 +0200

    Update GuidWrapperPropertyDrawer.cs

--- Editor/GuidWrapperPropertyDrawer.cs
+++ Editor/GuidWrapperPropertyDrawer.cs
@@ -1,24 +1,24 @@
-﻿using Drboum.Utilities.Runtime;
-using UnityEditor;
-using UnityEditor.UIElements;
-using UnityEngine.UIElements;
-
-namespace Drboum.Utilities.Editor
-{
-    [CustomPropertyDrawer(typeof(GuidWrapper))]
-    public class GuidWrapperPropertyDrawer : PropertyDrawer
-    {
-        // public override VisualElement CreatePropertyGUI(SerializedProperty property)
-        // {
-        //     // Create property container element.
-        //     var container = new VisualElement();
-        //
-        //     // Create property fields.
-        //     var guidDisplay = new PropertyField(property);
-        //     // Add fields to the container.
-        //     container.Add(guidDisplay);
-        //
-        //     return container;
-        // }
-    }
-}
+﻿// using Drboum.Utilities.Runtime;
+// using UnityEditor;
+// using UnityEditor.UIElements;
+// using UnityEngine.UIElements;
+//
+// namespace Drboum.Utilities.Editor
+// {
+//     [CustomPropertyDrawer(typeof(GuidWrapper))]
+//     public class GuidWrapperPropertyDrawer : PropertyDrawer
+//     {
+//         public override VisualElement CreatePropertyGUI(SerializedProperty property)
+//         {
+//             // Create property container element.
+//             var container = new VisualElement();
+//
+//             // Create property fields.
+//             var guidDisplay = new PropertyField(property);
+//             // Add fields to the container.
+//             container.Add(guidDisplay);
+//
+//             return container;
+//         }
+//     }
+// }
