c541efe303f5c357ab0a10e29a1764e3c18d934e
etienneR <55545442+Nyert@users.noreply.github.com>
2022-09-17 17:21:26 +0200

    guid readonly in editor

--- Runtime/EditorHybrid/AssetReferenceID.cs
+++ Runtime/EditorHybrid/AssetReferenceID.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Drboum.Utilities.Runtime.Attributes;
 using Unity.Mathematics;
 using UnityEngine;
+
 namespace Drboum.Utilities.Runtime.EditorHybrid
 {
     public interface IAssetReferenceID
@@ -16,11 +18,12 @@ namespace Drboum.Utilities.Runtime.EditorHybrid
     public abstract class AssetReferenceID : EditorCallBackScriptableObject<AssetReferenceID>, IAssetReferenceID, IEquatable<AssetReferenceID>
     {
         public const string AssetReferenceExtension = ".asset";
-        
-        [SerializeField] protected GuidWrapper _guid;
+
+        [SerializeField, InspectorReadOnly] protected GuidWrapper _guid;
 
         internal bool IsValidGuid => !default(GuidWrapper).Equals(_guid);
         public virtual bool IsValidAsset => IsValidGuid;
+
         public GuidWrapper Guid {
             get => _guid;
             internal set => _guid = value;
@@ -28,17 +31,17 @@ namespace Drboum.Utilities.Runtime.EditorHybrid
 
         public bool Equals(AssetReferenceID other)
         {
-            if (other is null )
+            if ( other is null )
             {
                 return false;
             }
-            if (  ReferenceEquals(this, other) )
+            if ( ReferenceEquals(this, other) )
             {
                 return true;
             }
             return _guid == other._guid;
         }
-        
+
         public override bool Equals(object obj) => obj is AssetReferenceID assetReferenceID && Equals(assetReferenceID);
 
         public override int GetHashCode() => Guid.GetHashCode();
