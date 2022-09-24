255a189c9c9fcabb65d05b8b3b56faa94389573a
etienneR <55545442+Nyert@users.noreply.github.com>
2022-09-24 16:54:52 +0200

    Update AssetReferenceIDAllPostprocessor.cs

--- Editor/AssetReferenceIDAllPostprocessor.cs
+++ Editor/AssetReferenceIDAllPostprocessor.cs
@@ -7,30 +7,6 @@ namespace Drboum.Utilities.Editor
 #if !DISABLE_ASSETREF_POSTPROCESSOR
     class AssetReferenceIDAllPostprocessor : AssetPostprocessor
     {
-        class PostProcessor : AssetPostprocessor
-        {
-            private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
-            {
-                for ( var index = 0; index < importedAssets.Length; index++ )
-                {
-                    string str = importedAssets[index];
-                    if ( !CanBePrefab(str) )
-                        continue;
-
-                    var assetReferenceID = AssetDatabase.LoadAssetAtPath<PrefabIdentity>(str);
-                    if ( assetReferenceID != null )
-                    {
-                        AssetReferenceIDBaseManager<TAssetInstance>.Instance.FixAssetIDIfInvalid(instance);
-
-                    }
-                }
-            }
-
-            private static bool CanBePrefab(string str)
-            {
-                return Equals(Path.GetExtension(str), ".prefab");
-            }
-        }
 
         private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
