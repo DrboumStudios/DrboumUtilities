cfe2fa9b4866950846fc8bb44ac13680bf4b6912
etienneR <55545442+Nyert@users.noreply.github.com>
2022-09-25 22:34:22 +0200

    Update PrefabIdentity.cs

--- Runtime/EditorHybrid/PrefabIdentity.cs
+++ Runtime/EditorHybrid/PrefabIdentity.cs
@@ -1,27 +1,24 @@
-using System.IO;
 using Drboum.Utilities.Runtime.Attributes;
-using Unity.Properties;
-using UnityEditor;
 using UnityEngine;
 
 namespace Drboum.Utilities.Runtime.EditorHybrid
 {
-    public class PrefabIdentity : MonoBehaviour,IAssetReferenceID
+    public class PrefabIdentity : MonoBehaviour, IAssetReferenceID
     {
-        [SerializeField, InspectorReadOnly, CreateProperty,InspectorName("PrefabGuid")] internal GuidWrapper _guid;
-        
+        [SerializeField, InspectorReadOnly] internal GuidWrapper _guid;
+
         public GuidWrapper Guid {
             get => _guid;
-            internal  set => _guid = value;
+            protected set => _guid = value;
         }
 
         GuidWrapper IAssetReferenceID.Guid {
-            get => this.Guid;
-            set => this.Guid = value;
+            get => _guid;
+            set => _guid = value;
         }
 
         public bool IsValidAsset => _guid.IsValid;
-        
-     
+
+
     }
 }
