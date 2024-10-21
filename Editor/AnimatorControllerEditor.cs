5642336dfb1f106099e0a0981f7effcba5e56c32
etienneR <55545442+Nyert@users.noreply.github.com>
2024-10-21 20:08:42 +0200

    animator parameter scriptable is now owned by an animatorController
    small code correction

--- Editor/AnimatorControllerEditor.cs
+++ Editor/AnimatorControllerEditor.cs
@@ -4,33 +4,39 @@ using Drboum.Utilities.Runtime.Animation;
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
-namespace Drboum.Utilities.Editor {
+
+namespace Drboum.Utilities.Editor
+{
     [CustomEditor(typeof(AnimatorController))]
-    public class AnimatorControllerEditor : UnityEditor.Editor {
+    public class AnimatorControllerEditor : UnityEditor.Editor
+    {
         public string destinationFolder;
 
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
             var animatorController = (AnimatorController)target;
+            if ( string.IsNullOrWhiteSpace(destinationFolder) )
+            {
+                destinationFolder = animatorController.name + "/";
+            }
             destinationFolder = EditorGUILayout.TextField(destinationFolder);
-            if ( GUILayout.Button("Export animator parameters") ) {
-                string assetPath   = AssetDatabase.GetAssetPath(animatorController.GetInstanceID());
+            if ( GUILayout.Button("Export animator parameters") )
+            {
+                string assetPath = AssetDatabase.GetAssetPath(animatorController.GetInstanceID());
                 string rootDirName = Path.GetDirectoryName(assetPath);
-                if ( string.IsNullOrWhiteSpace(destinationFolder) ) {
-                    destinationFolder = animatorController.name;
-                }
                 string newFolder = Path.Combine(rootDirName, destinationFolder);
-                if ( !AssetDatabase.IsValidFolder(newFolder) ) {
+                if ( !AssetDatabase.IsValidFolder(newFolder) )
+                {
                     AssetDatabase.CreateFolder(rootDirName, destinationFolder);
                 }
-
-                foreach ( AnimatorControllerParameter item in animatorController.parameters ) {
-                    string newfilePath  = Path.Combine(newFolder, item.name + ".asset");
-                    var    newParameter = CreateInstance<AnimatorParameter>();
-                    Debug.Log(newfilePath);
-                    newParameter.Initialize(item);
-                    AssetDatabase.CreateAsset(newParameter, newfilePath);
+                foreach ( AnimatorControllerParameter item in animatorController.parameters )
+                {
+                    string newFilePath = Path.Combine(newFolder, item.name + ".asset");
+                    var newParameter = CreateInstance<AnimatorParameter>();
+                    Debug.Log(newFilePath);
+                    newParameter.Initialize(item, animatorController);
+                    AssetDatabase.CreateAsset(newParameter, newFilePath);
                 }
             }
         }
--- Runtime/Animation/AnimatorParameter.cs
+++ Runtime/Animation/AnimatorParameter.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using Drboum.Utilities.Runtime.Attributes;
+using UnityEditor.Animations;
 using UnityEngine;
 
-namespace Drboum.Utilities.Runtime.Animation {
+namespace Drboum.Utilities.Runtime.Animation
+{
 #if UNITY_EDITOR
-    [CreateAssetMenu(fileName = nameof(AnimatorParameter), menuName = nameof(Drboum)+"/" +nameof(Utilities)+ "/"+nameof(Animation)+"/"+nameof(AnimatorParameter))]
+    [CreateAssetMenu(fileName = nameof(AnimatorParameter), menuName = nameof(Drboum) + "/" + nameof(Utilities) + "/" + nameof(Animation) + "/" + nameof(AnimatorParameter))]
 #endif
-    public class AnimatorParameter : ScriptableObject, IAnimatorParameter,
-        IEquatable<AnimatorParameter> {
+    public class AnimatorParameter : ScriptableObject, IAnimatorParameter, IEquatable<AnimatorParameter>
+    {
+        [SerializeField] protected string parameterName;
+        [SerializeField] protected AnimatorControllerParameterType parameterType;
+        [SerializeField] [InspectorReadOnly] protected int hashId;
+#if UNITY_EDITOR
+        [SerializeField] [InspectorReadOnly] private UnityEditor.Animations.AnimatorController animatorController;
+#endif
 
-        [SerializeField]                     protected string                          parameterName;
-        [SerializeField]                     protected AnimatorControllerParameterType parametertype;
-        [SerializeField] [InspectorReadOnly] protected int                             _hashId;
-        [SerializeField] [InspectorReadOnly] private   uint                            uObjectID;
-
-        public string                          ParameterName => parameterName;
-        public int                             HashId        => _hashId;
-        public AnimatorControllerParameterType ParameterType => parametertype;
+        public string ParameterName => parameterName;
+        public int HashId => hashId;
+        public AnimatorControllerParameterType ParameterType => parameterType;
 
         /// <summary>
         ///     In order for 2 <see cref="AnimatorParameter" /> to have be equals, the presence of the <see cref="Animator" /> is
@@ -27,26 +30,33 @@ namespace Drboum.Utilities.Runtime.Animation {
         /// <returns></returns>
         public bool Equals(AnimatorParameter other)
         {
-            if ( ReferenceEquals(other, null) ) {
+            if ( ReferenceEquals(other, null) )
+            {
                 return false;
             }
-            if ( ReferenceEquals(other, null) ) {
+            if ( ReferenceEquals(other, null) )
+            {
                 return false;
             }
             return IsEquals(this, other);
         }
-        public uint UObjectID    { get => uObjectID; set => uObjectID = value; }
-        public void Initialize() { }
+
 #if UNITY_EDITOR
+        public UnityEditor.Animations.AnimatorController AnimatorController => animatorController;
+
         [ContextMenu(nameof(GenerateHashId))]
 #endif
         public void GenerateHashId()
         {
-            _hashId = GenerateHashId(parameterName);
+            hashId = GenerateHashId(parameterName);
         }
+
+        public void Initialize() { }
+
         public void ResetToDefaultValue(Animator animator)
         {
-            switch ( ParameterType ) {
+            switch ( ParameterType )
+            {
                 case AnimatorControllerParameterType.Float:
                     UpdateAnimator(animator, default(float));
                     break;
@@ -59,55 +69,67 @@ namespace Drboum.Utilities.Runtime.Animation {
             }
 
         }
+
         public void UpdateAnimator(Animator animator, bool value)
         {
-            if ( animator != null ) {
-                animator.SetBool(_hashId, value);
+            if ( animator != null )
+            {
+                animator.SetBool(hashId, value);
             }
         }
 
         public void UpdateAnimator(Animator animator, float value)
         {
-            if ( animator != null ) {
-                animator.SetFloat(_hashId, value);
+            if ( animator != null )
+            {
+                animator.SetFloat(hashId, value);
             }
         }
+
         public void UpdateAnimator(Animator animator, float value, float dampTime, float deltaTime)
         {
-            if ( animator != null ) {
-                animator.SetFloat(_hashId, value, dampTime, deltaTime);
+            if ( animator != null )
+            {
+                animator.SetFloat(hashId, value, dampTime, deltaTime);
             }
         }
+
         public void UpdateAnimator(Animator animator, int value)
         {
-            if ( animator != null ) {
-                animator.SetInteger(_hashId, value);
+            if ( animator != null )
+            {
+                animator.SetInteger(hashId, value);
             }
         }
 
         public void UpdateAnimatorTrigger(Animator animator)
         {
-            if ( animator != null ) {
-                animator.SetTrigger(_hashId);
+            if ( animator != null )
+            {
+                animator.SetTrigger(hashId);
             }
         }
 
         public bool GetBool(Animator animator)
         {
-            return animator != null && animator.GetBool(_hashId);
+            return animator != null && animator.GetBool(hashId);
         }
+
         public int GetInt(Animator animator)
         {
-            if ( animator != null ) {
-                return animator.GetInteger(_hashId);
+            if ( animator != null )
+            {
+                return animator.GetInteger(hashId);
             }
 
             return default;
         }
+
         public float GetFloat(Animator animator)
         {
-            if ( animator != null ) {
-                return animator.GetFloat(_hashId);
+            if ( animator != null )
+            {
+                return animator.GetFloat(hashId);
             }
 
             return default;
@@ -115,11 +137,13 @@ namespace Drboum.Utilities.Runtime.Animation {
 
         public override bool Equals(object obj)
         {
-            if ( obj == null ) {
+            if ( obj == null )
+            {
                 return false;
             }
             var objAsPart = obj as AnimatorParameter;
-            if ( objAsPart == null ) {
+            if ( objAsPart == null )
+            {
                 return false;
             }
             return Equals(objAsPart);
@@ -134,28 +158,30 @@ namespace Drboum.Utilities.Runtime.Animation {
         {
             var paramData = new AnimatorParameterSerializedData();
             paramData.AnimatorParameter = this;
-            switch ( parametertype ) {
+            switch ( parameterType )
+            {
                 case AnimatorControllerParameterType.Float:
-                    paramData.SetValue(animator.GetFloat(_hashId));
+                    paramData.SetValue(animator.GetFloat(hashId));
                     break;
                 case AnimatorControllerParameterType.Int:
-                    paramData.SetValue(animator.GetInteger(_hashId));
+                    paramData.SetValue(animator.GetInteger(hashId));
                     break;
                 case AnimatorControllerParameterType.Bool:
-                    paramData.SetValue(animator.GetBool(_hashId));
+                    paramData.SetValue(animator.GetBool(hashId));
                     break;
             }
             return paramData;
         }
+
         public static int GenerateHashId(string parameterName)
         {
             return Animator.StringToHash(parameterName);
         }
 
         public static bool IsEquals(IAnimatorParameter animatorParameterA,
-            IAnimatorParameter                         animatorParameterB)
+            IAnimatorParameter animatorParameterB)
         {
-            return animatorParameterA.HashId        == animatorParameterB.HashId &&
+            return animatorParameterA.HashId == animatorParameterB.HashId &&
                    animatorParameterA.ParameterType == animatorParameterB.ParameterType;
         }
 
@@ -173,27 +199,30 @@ namespace Drboum.Utilities.Runtime.Animation {
 
 
 #if UNITY_EDITOR
-        protected bool _initialized;
-        public void Initialize(AnimatorControllerParameter animatorControllerParameter)
+        internal void Initialize(AnimatorControllerParameter animatorControllerParameter, AnimatorController animatorController)
         {
-            if ( !_initialized && animatorControllerParameter != null ) {
-                _initialized  = true;
+            if ( animatorControllerParameter != null )
+            {
                 parameterName = animatorControllerParameter.name;
-                _hashId       = animatorControllerParameter.nameHash;
-                parametertype = animatorControllerParameter.type;
+                hashId = animatorControllerParameter.nameHash;
+                parameterType = animatorControllerParameter.type;
+                this.animatorController = animatorController;
             }
         }
 
         private void OnValidate()
         {
-            if ( !string.IsNullOrEmpty(parameterName) ) {
+            if ( !string.IsNullOrEmpty(parameterName) )
+            {
                 GenerateHashId();
             }
         }
 #endif
     }
-    public interface IAnimatorParameter {
-        int                             HashId        { get; }
+
+    public interface IAnimatorParameter
+    {
+        int HashId { get; }
         AnimatorControllerParameterType ParameterType { get; }
     }
 }
