f7b202e6cae729fcee982917b9707d8307b0c1af
etienneR <55545442+Nyert@users.noreply.github.com>
2025-02-10 15:25:21 +0100

    change injection folder name add unity.collections access to internals add method for nativereference go get the value as a ref

---
+++ AssemblyInjection/Collections/EntityManagerExt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+public static unsafe class CollectionInternalExtensions
+{
+    public static ref T AsRef<T>(this ref NativeReference<T> nativeReference)
+        where T : unmanaged
+    {
+#if ENABLE_UNITY_COLLECTIONS_CHECKS
+        AtomicSafetyHandle.CheckWriteAndThrow(nativeReference.m_Safety);
+#endif
+        return ref UnsafeUtility.AsRef<T>(nativeReference.m_Data);
+    }
+}
