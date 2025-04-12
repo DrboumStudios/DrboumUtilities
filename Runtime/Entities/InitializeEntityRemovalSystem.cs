e390a3f58602682f34c37917d8e1839aeba3024b
etienneR <55545442+Nyert@users.noreply.github.com>
2025-04-12 16:50:24 +0200

    add InitializeEntity Component and System

---
+++ Runtime/Entities/InitializeEntityRemovalSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+
+namespace Drboum.Utilities.Entities
+{
+    [UpdateInGroup(typeof(InitializationSystemGroup), OrderFirst = true)]
+    [UpdateBefore(typeof(BeginInitializationEntityCommandBufferSystem))]
+    internal partial struct InitializeEntityRemovalSystem : ISystem
+    {
+        [BurstCompile]
+        void ISystem.OnUpdate(ref SystemState state)
+        {
+            var allJustSpawnedQuery = SystemAPI.QueryBuilder()
+                .WithAll<InitializeEntityTag>()
+                .Build();
+            state.EntityManager.RemoveComponent<InitializeEntityTag>(allJustSpawnedQuery);
+        }
+    }
+
+    public struct InitializeEntityTag : IComponentData
+    { }
+}
