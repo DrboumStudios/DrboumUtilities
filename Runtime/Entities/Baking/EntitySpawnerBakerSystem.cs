74adcfe726d89ab8cf6155c997bbc8ac1461040b
etienneR <55545442+Nyert@users.noreply.github.com>
2025-07-19 16:57:26 +0200

    fix error

--- Runtime/Entities/Baking/EntitySpawnerBakerSystem.cs
+++ Runtime/Entities/Baking/EntitySpawnerBakerSystem.cs
@@ -3,6 +3,7 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
+using Unity.Entities.Hybrid;
 using Unity.Mathematics;
 using Unity.Profiling;
 using Unity.Transforms;
