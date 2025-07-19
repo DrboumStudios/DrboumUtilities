2648b6a084ca335d79d0abbdbede500b060160c3
etienneR <55545442+Nyert@users.noreply.github.com>
2025-07-19 15:45:49 +0200

    add multiple apis with coherent names for the physics shape authoring

--- AssemblyInjection/Physics/Custom/PhysicsInternals.cs
+++ AssemblyInjection/Physics/Custom/PhysicsInternals.cs
@@ -1,10 +1,24 @@
-﻿namespace Unity.Physics.Authoring
+﻿using Unity.Mathematics;
+
+namespace Unity.Physics.Authoring
 {
     public static class PhysicsInternals
     {
         public static CapsuleGeometry GetBakedCapsuleRuntime(this PhysicsShapeAuthoring shapeAuthoring)
         {
             return shapeAuthoring.GetBakedCapsuleProperties().ToRuntime();
+        }
+        public static SphereGeometry GetBakedSphereRuntime(this PhysicsShapeAuthoring shapeAuthoring)
+        {
+            return shapeAuthoring.GetBakedSphereProperties(out _);
+        }
+        public static BoxGeometry GetBakedBoxRuntime(this PhysicsShapeAuthoring shapeAuthoring)
+        {
+            return shapeAuthoring.GetBakedBoxProperties();
+        }
+        public static CylinderGeometry GetBakedCylinderRuntime(this PhysicsShapeAuthoring shapeAuthoring)
+        {
+            return shapeAuthoring.GetBakedCylinderProperties();
         }
     }
 }
