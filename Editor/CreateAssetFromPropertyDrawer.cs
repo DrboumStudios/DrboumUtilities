028331fdcd1dc46f1b018e6038929617052ea1a0
etienneR <55545442+Nyert@users.noreply.github.com>
2025-12-18 20:44:43 +0100

    Update CreateAssetFromPropertyDrawer.cs

--- Editor/CreateAssetFromPropertyDrawer.cs
+++ Editor/CreateAssetFromPropertyDrawer.cs
@@ -18,7 +18,7 @@ namespace Drboum.Utilities.Editor
             Object parentObject = GetPropertyData(property, out var propertyFieldInfo, out var createButtonAttribute);
             // we have an attribute that overrides the default behavior -> let the other drawer do its job
             if ( createButtonAttribute != null )
-                return new VisualElement();
+                return base.CreatePropertyGUI(property);
 
             return BuildVisualElements(property, parentObject, propertyFieldInfo.FieldType, default(DefaultCreateScriptableObjectInstance), default(DefaultSavePersistentAsset));
         }
