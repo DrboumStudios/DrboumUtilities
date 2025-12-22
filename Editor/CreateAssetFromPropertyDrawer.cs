354ac0a7378a53770bfc0199068ddef1a4f6792e
etienneR <55545442+Nyert@users.noreply.github.com>
2025-12-22 17:19:06 +0100

    cleanup

--- Editor/CreateAssetFromPropertyDrawer.cs
+++ Editor/CreateAssetFromPropertyDrawer.cs
@@ -12,20 +12,6 @@ using Object = UnityEngine.Object;
 
 namespace Drboum.Utilities.Editor
 {
-    [CustomPropertyDrawer(typeof(ScriptableObject), true)]
-    public class CreateScriptableObjectByDefaultFromPropertyDrawer : CreateAssetFromPropertyDrawer
-    {
-        public override VisualElement CreatePropertyGUI(SerializedProperty property)
-        {
-            Object parentObject = GetPropertyData(property, out var propertyFieldInfo, out var createButtonAttribute);
-            // we have an attribute that overrides the default behavior -> let the other drawer do its job
-            if ( createButtonAttribute != null )
-                return base.CreatePropertyGUI(property);
-
-            return BuildVisualElements(property, parentObject, propertyFieldInfo?.FieldType, default(DefaultCreateScriptableObjectInstance), default(DefaultSavePersistentAsset));
-        }
-    }
-
     [CustomPropertyDrawer(typeof(CreateAssetFromPropertyAttribute))]
     public class CreateAssetFromPropertyDrawer : PropertyDrawer
     {
