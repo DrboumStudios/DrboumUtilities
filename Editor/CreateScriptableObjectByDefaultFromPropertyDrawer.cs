8833c94efb612220a0e0136ac986670b432285cd
etienneR <55545442+Nyert@users.noreply.github.com>
2025-12-22 17:28:05 +0100

    Update CreateScriptableObjectByDefaultFromPropertyDrawer.cs

--- Editor/CreateScriptableObjectByDefaultFromPropertyDrawer.cs
+++ Editor/CreateScriptableObjectByDefaultFromPropertyDrawer.cs
@@ -5,17 +5,17 @@ using UnityEngine.UIElements;
 
 namespace Drboum.Utilities.Editor
 {
-    [CustomPropertyDrawer(typeof(ScriptableObject), true)]
-    public class CreateScriptableObjectByDefaultFromPropertyDrawer : CreateAssetFromPropertyDrawer
-    {
-        public override VisualElement CreatePropertyGUI(SerializedProperty property)
-        {
-            Object parentObject = GetPropertyData(property, out var propertyFieldInfo, out var createButtonAttribute);
-            // we have an attribute that overrides the default behavior -> let the other drawer do its job
-            if ( createButtonAttribute != null )
-                return base.CreatePropertyGUI(property);
-
-            return BuildVisualElements(property, parentObject, propertyFieldInfo?.FieldType, default(DefaultCreateScriptableObjectInstance), default(DefaultSavePersistentAsset));
-        }
-    }
+    // [CustomPropertyDrawer(typeof(ScriptableObject), true)]
+    // public class CreateScriptableObjectByDefaultFromPropertyDrawer : CreateAssetFromPropertyDrawer
+    // {
+    //     public override VisualElement CreatePropertyGUI(SerializedProperty property)
+    //     {
+    //         Object parentObject = GetPropertyData(property, out var propertyFieldInfo, out var createButtonAttribute);
+    //         // we have an attribute that overrides the default behavior -> let the other drawer do its job
+    //         if ( createButtonAttribute != null )
+    //             return base.CreatePropertyGUI(property);
+    //
+    //         return BuildVisualElements(property, parentObject, propertyFieldInfo?.FieldType, default(DefaultCreateScriptableObjectInstance), default(DefaultSavePersistentAsset));
+    //     }
+    // }
 }
