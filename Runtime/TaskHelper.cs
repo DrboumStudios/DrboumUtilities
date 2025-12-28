a2da10f0af1c51ea9d75653f6bc7ced0e2504112
etienneR <55545442+Nyert@users.noreply.github.com>
2025-12-28 16:00:24 +0100

    Update TaskHelper.cs

--- Runtime/TaskHelper.cs
+++ Runtime/TaskHelper.cs
@@ -2,33 +2,30 @@
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Drboum.Utilities
+public static class TaskHelper
 {
-    public static class TaskHelper
+    public static Task LogException(this Task task)
     {
-        public static Task LogException(this Task task)
-        {
-            task.ContinueWith(LogTaskException);
-            return task;
-        }
+        task.ContinueWith(LogTaskException);
+        return task;
+    }
 
-        public static Task<T> LogException<T>(this Task<T> task)
-        {
-            task.ContinueWith(LogTaskException);
-            return task;
-        }
+    public static Task<T> LogException<T>(this Task<T> task)
+    {
+        task.ContinueWith(LogTaskException);
+        return task;
+    }
 
-        private static void LogTaskException(Task t)
+    private static void LogTaskException(Task t)
+    {
+        if ( t.IsFaulted && t.Exception != null )
         {
-            if ( t.IsFaulted && t.Exception != null )
+            Exception exception = t.Exception;
+            while ( exception.InnerException != null )
             {
-                Exception exception = t.Exception;
-                while ( exception.InnerException != null )
-                {
-                    exception = exception.InnerException;
-                }
-                Debug.LogException(exception);
+                exception = exception.InnerException;
             }
+            Debug.LogException(exception);
         }
     }
 }
